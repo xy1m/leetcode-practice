3b1959deedf459327619b27acd6ebcf7edb7c3cd
George Zhenpeng <zhangzhpeng@gmail.com>
2018-06-08 13:58:25 -0700

    Add inital commit

---
+++ src/main/java/com/xy1m/App.java
@@ -0,0 +1,10 @@
+package com.xy1m;
+
+/**
+ * Created by gzhenpeng on 6/8/18
+ */
+public class App {
+    public static void main(String... args) {
+        System.out.println("Hello World!o");
+    }
+}
